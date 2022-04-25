8f3c98de957bcee91cbf55be2e151c924a995a37
Alexy <alexy.ib@outlook.com>
2022-04-25 13:45:15 +0300

    wip

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.3"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.1"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.1"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.2.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.5.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
